845ae323598fc4af11fd9880a7dac0965c61f944
Gisele-Santana98 <gisy.ccb@gmail.com>
2024-10-02 17:16:39 -0300

    Subindo pageNeurona.

---
+++ dados.js
@@ -0,0 +1,37 @@
+let dados = [
+{
+    titulo : "Alzheimer",
+    descricao : "É uma forma comum de demência que se caracteriza por uma deterioração progressiva das funções cognitivas. Essa condição neurodegenerativa afeta principalmente a memória, a linguagem, o raciocínio e a capacidade de realizar tarefas cotidianas.Os sintomas geralmente se manifestam de forma gradual e pioram com o tempo, impactando significativamente a qualidade de vida do indivíduo e de seus familiares.",
+    sintomas :"",
+    tratamento : "",
+    link : "https://www.gov.br/saude/pt-br/assuntos/saude-de-a-a-z/a/alzheimer",
+    tags: "demência esquecimento alzheimer perda de memória."
+},
+{
+    titulo : "Enxaqueca",
+    descricao : "A enxaqueca é um tipo de dor de cabeça recorrente que pode ser bastante intensa e incapacitante. Diferente de uma simples dor de cabeça,",
+    sintomas :"",
+    tratamento : "",
+    link : "https://www.msdmanuals.com/pt-br/casa/dist%C3%BArbios-cerebrais-da-medula-espinal-e-dos-nervos/cefaleias/enxaquecas?autoredirectid=24675",
+    tags: "dor de cabeça incapacitante dor intensa recorrente enxaqueca."
+},
+{
+    titulo : "Doença de Parkinson",
+    descricao : "A Doença de Parkinson é uma condição neurológica crônica que afeta principalmente o movimento. Ela ocorre devido à degeneração das células nervosas em uma região específica do cérebro, a substância negra. Essas células produzem dopamina, um neurotransmissor fundamental para o controle dos movimentos.",
+    sintomas :"",
+    tratamento : "",
+    link : "https://www.msdmanuals.com/pt-br/profissional/dist%C3%BArbios-neurol%C3%B3gicos/transtornos-de-movimento-e-cerebelares/doen%C3%A7a-de-parkinson",
+    tags: "movimento dopamina degeneração neurotransmissor."
+},
+{
+    titulo : "Esclerose Múltipla",
+    descricao : "A Esclerose Múltipla (EM) é uma doença crônica do sistema nervoso central que afeta a mielina, uma substância que reveste e protege os neurônios. Essa doença é autoimune, ou seja, o próprio sistema imunológico ataca as células do organismo, no caso, a mielina.",
+    sintomas :"",
+    tratamento : "",
+    link : "https://www.einstein.br/doencas-sintomas/esclerose-multipla-em" ,
+    tags: "esclerose sutpimune mielina sistemana nervose central."
+}
+];
+
+
+    
